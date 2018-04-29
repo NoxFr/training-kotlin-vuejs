6adaca59640c702cffba17e319cd8032168f64ce
Mathieu Durand <mat.durand@orange.fr>
2018-04-29 11:34:28 +0200

    :tada: add vuetify (materials ui)

--- vuejs-front/src/main.js
+++ vuejs-front/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+
+// css resources
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
