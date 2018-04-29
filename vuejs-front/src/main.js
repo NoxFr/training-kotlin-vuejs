f0c3ff0f2230604f9dfe189293606e4f763ee166
Mathieu Durand <mat.durand@orange.fr>
2018-04-29 18:39:52 +0200

    :bug: using @ path

--- vuejs-front/src/main.js
+++ vuejs-front/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import Vuetify from 'vuetify'
 import fontawesome from '@fortawesome/fontawesome'
 import brands from '@fortawesome/fontawesome-free-brands'
