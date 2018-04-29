8fa8cc1b4082c8ccaa0b28ad6ae77062b9ff7635
Mathieu Durand <mat.durand@orange.fr>
2018-04-29 21:17:55 +0200

    :bug: add primitive key

--- vuejs-front/src/components/Footer.vue
+++ vuejs-front/src/components/Footer.vue
@@ -3,7 +3,7 @@
       <v-layout row wrap justify-center class="grey darken-3">
           <v-btn
             v-for="icon in icons"
-            :key="icon"
+            :key="icon.id"
             icon
             v-bind:href="icon.link"
           >
@@ -21,14 +21,17 @@ export default {
   name: 'tkvFooter',
   data: () => ({
     icons: [ {
+      id: 1,
       class: 'fab fa-github',
       link: 'https://github.com/NoxFr/training-kotlin-vuejs'
     },
     {
+      id: 2,
       class: 'fab fa-twitter',
       link: 'http://twitter.com/NoxFr'
     },
     {
+      id: 3,
       class: 'fab fa-linkedin',
       link: 'https://www.linkedin.com/in/mathieu-durand-260104137/'
     }]
